d9f2ffa0f2b8b52282c4ec895368388911b718a1
mamun <mamunopstel@gmail.com>
2024-11-23 11:59:38 +0600

    updated the 'change active tab button' js file to change active class to both normal view and icon only view sidebar. The HTML structure of both 'menu_btns_box' is important for this change to work properly. lastly fixed some styles for the sidebar of icon view

--- asset/js/functions/Change_Active_TabButton.js
+++ asset/js/functions/Change_Active_TabButton.js
@@ -1,21 +1,53 @@
-// <===== Changing the active class between the sidemenu buttons =====>
+// =====> New Version To Change Active Class To Both View Sidebar <=====
+// =====> HTML Structure is important to maintain in order to work properly <=====
 
-const Button_box = document
-  .querySelector(".menu_btns_box")
-  .addEventListener("click", (event) => {
-    const buttons_list = Array.from(document.querySelectorAll(".menu_btns"));
-    const sidebar = document.querySelector(".side_bar");
+// Getting the buttons boxes
+const button_box_1 = document.querySelector(".normal_view_box .menu_btns_box");
+const button_box_2 = document.querySelector(".icon_view_box .menu_btns_box");
 
-    if (event.target.classList.contains("menu_btns")) {
-      buttons_list.forEach((button) => {
-        button.classList.remove("active");
-      });
-      event.target.classList.add("active");
-      console.log("Changed the active buttton")
-      // Closing the sidebar after changing the tab
-      sidebar.classList.toggle("side_bar_active");
-    }
+// Getting the buttons lists
+const buttons_list_1 = Array.from(
+  document.querySelectorAll(".normal_view_box .menu_btns")
+);
+const buttons_list_2 = Array.from(
+  document.querySelectorAll(".icon_view_box .menu_btns")
+);
+
+const changeAvtivebutton = (index) => {
+  buttons_list_1.forEach((button) => {
+    button.classList.remove("active");
   });
+  buttons_list_1[index].classList.add("active");
 
-const buttons_list = Array.from(document.querySelectorAll(".menu_btns"));
-console.log(buttons_list)
+  buttons_list_2.forEach((button) => {
+    button.classList.remove("active");
+  });
+  buttons_list_2[index].classList.add("active");
+};
+
+const getButtonIndex = (event, buttons_list) => {
+  // Check if the clicked target is one of the buttons
+  if (
+    event.target.classList.contains("menu_btns") ||
+    event.target.closest(".menu_btns")
+  ) {
+    // Get the button element (in case the <i> tag is clicked)
+    const button = event.target.classList.contains("menu_btns")
+      ? event.target
+      : event.target.closest(".menu_btns");
+
+    // Get the index of the clicked button in the buttons list provided as an argument
+    const index = buttons_list.indexOf(button);
+
+    changeAvtivebutton(index);
+  }
+};
+
+// Adding an event listener to the button box 1
+button_box_1.addEventListener("click", (event) => {
+  getButtonIndex(event, buttons_list_1);
+});
+// Adding an event listener to the button box 2
+button_box_2.addEventListener("click", (event) => {
+  getButtonIndex(event, buttons_list_2);
+});
